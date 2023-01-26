29411ed9e49aeb368d1aa2ad0c16eb2f56b19c4c
Phan Long <phanlong@MacBook-Pro-cua-Phan.local>
2023-01-26 18:25:46 +0700

    remove

--- src/helpers/firebase.config.ts
+++
@@ -1,6 +0,0 @@
-import { registerAs } from '@nestjs/config';
-import { readFileSync } from 'fs';
-
-export default registerAs('firebase', () => {
-  return JSON.parse(readFileSync('src/helpers/firebaseconf.json', 'utf8'));
-});
